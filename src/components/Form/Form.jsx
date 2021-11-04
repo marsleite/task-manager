258c4c934c56117cde925f6ead7eaabbd2efb70b
Marcelo <marsleite@gmail.com>
2021-11-04 09:43:04 -0300

    configurando o forms com os devidos inputs

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,1 +1,15 @@
-// componente de formulario para criar e editar tasks
+import React from 'react';
+
+const Form = () => {
+  return (
+    <div>
+      <form onSubmit={ handleSubmit }>
+        <input type="text" name="criador" placeholder="Cridor" />
+        <input type="text" name="titulo" placeholder="título" />
+        <input type="text" name="mensagem" placeholder="mensagem" />
+        <input type="text" name="tags" placeholder="tags" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
