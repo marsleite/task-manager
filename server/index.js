4446b5bfada971acf76e9950d6452f0e489c3488
Marcelo <marsleite@gmail.com>
2021-11-04 11:20:57 -0300

    conexão com o banco de dados mongodb cloud atlas

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
@@ -6,3 +8,12 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(cors());
+
+const CONNECT_DB_URL = process.env.CONNECT_DB;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECT_DB_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
+  .catch(err => console.log(err.message));
+
+module.exports = app;
