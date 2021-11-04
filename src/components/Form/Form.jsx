f5cd55f447d59c8206e39668a2f62391a49b392b
Marcelo <marsleite@gmail.com>
2021-11-04 09:59:20 -0300

    criação do estado com useState, função onChange e esboço da função handlesubmit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(postData);
+  };
+
   return (
     <div>
       <form onSubmit={ handleSubmit }>
-        <input type="text" name="criador" placeholder="Cridor" />
-        <input type="text" name="titulo" placeholder="título" />
-        <input type="text" name="mensagem" placeholder="mensagem" />
-        <input type="text" name="tags" placeholder="tags" />
-        <input type="submit" value="Submit" />
+        <input 
+          type="text" 
+          name="criador" 
+          placeholder="Cridor" 
+          value={ postData.creator } 
+          onChange={ ({ target }) => setPostData({ ...postData, creator: target.value }) }
+        />
+        <input 
+          type="text" 
+          name="titulo" 
+          placeholder="título" 
+          value={ postData.title } 
+          onChange={ ({ target }) => setPostData({ ...postData, title: target.value }) }
+        />
+        <input 
+          type="text" 
+          name="mensagem" 
+          placeholder="mensagem" 
+          value={ postData.message } 
+          onChange={ ({ target }) => setPostData({ ...postData, message: target.value }) }
+        />
+        <input 
+          type="text" 
+          name="tags" 
+          placeholder="tags" 
+          value={ postData.tags } 
+          onChange={ ({ target }) => setPostData({ ...postData, tags: target.value }) }
+        />
+        <button type="submit">Enviar</button>
       </form>
     </div>
   );
 }
+
+export default Form;
